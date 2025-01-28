0d3459a1dff01828b9564cf8e5ff4dee19162f1f
MartinFalconL <falcon.martin@gmail.com>
2025-01-28 19:32:21 +0100

    agrego ejemplo de random con la bola magica

--- Magicball.py
+++ Magicball.py
@@ -1,3 +1,4 @@
+# The program will randomly select a number between 1 and 8 and then print the corresponding answer.
 import random
 question = input("Ask the magic 8 ball a question: ")
 answers = random.randint(1, 8)
@@ -19,17 +20,3 @@ elif answers == 8:
     print("My sources say no")  
 else:
     print("Invalid question")
-# The program will randomly select a number between 1 and 8 and then print the corresponding answer.
-# The program will print "Invalid question" if the user does not ask a question.
-# The program will print "Ask again later" if the number 4 is selected.
-# The program will print "My reply is no" if the number 7 is selected.
-# The program will print "My sources say no" if the number 8 is selected.
-# The program will print "It is certain" if the number 1 is selected.
-# The program will print "Outlook good" if the number 2 is selected.
-# The program will print "You may rely on it" if the number 3 is selected.
-# The program will print "Concentrate and ask again" if the number 5 is selected.
-# The program will print "Reply hazy, try again" if the number 6 is selected.
-# The program will print "Invalid question" if the user does not ask a question.
-# The program will print "Ask again later" if the number 4 is selected.
-# The program will print "My reply is no" if the number 7 is selected.
-# The program will print "My sources say no" if the number 8 is selected.

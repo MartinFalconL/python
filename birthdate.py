19c8041cfb3d2d3a217479c63489c845aabf1be2
MartinFalconL <falcon.martin@gmail.com>
2026-01-08 12:08:18 +0100

    New course from MIT

---
+++ birthdate.py
@@ -0,0 +1,8 @@
+# learning to print a result in order
+name = (input ("Enter your first name:"))
+apellido = (input ("Enter your last name:"))
+print ("enter your birth date:")
+month = (input ("(please type it) Month?:"))
+day = (input ("day?:"))
+year = (input ("year?:"))
+print (name, apellido, "was born on", month, day , year,)

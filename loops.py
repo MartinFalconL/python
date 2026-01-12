604295e28c79bf17b54b9802b3cbe780f92ff0cb
MartinFalconL <falcon.martin@gmail.com>
2026-01-12 11:34:15 +0100

    Added loops.py file with basic loop examples

--- loops.py
+++ loops.py
@@ -16,9 +16,28 @@
 
 #----------------------------------------
 #another way 
-countdown = int(input("type a staring countdown number:"))
-while (countdown < 0):
-    countdown = int(input("please type a number over zero:"))
-while (countdown > -1):
-    print (countdown)
-    countdown -= 1
+#countdown = int(input("type a staring countdown number:"))
+#while (countdown < 0):
+#    countdown = int(input("please type a number over zero:"))
+#while (countdown > -1):
+#    print (countdown)
+#    countdown -= 1
+
+#----------------------------------------
+#calculate exponentials using "for" (no validations, trusting user)
+#base = int(input("enter base number:"))
+#exp = int(input("enter exponential:"))
+#x = base
+#if (exp != 0):
+#    for i in range (1, exp):
+#        x = x * base
+#    print ("the result is", x)
+#else: print("the result is 1")
+
+#----------------------------------------
+#ask for a number and keep asking until the number is divisible by 2
+
+num = int (input ("Enter a number:"))
+while (num % 2 != 0):
+        num = int (input ("Enter a CORRECT number:"))
+print ("Good work!! You finally got it! :p")

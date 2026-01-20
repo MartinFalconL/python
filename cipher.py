bf32a3f3ced5d408363760f4640c9ff46aea437a
MartinFalconL <falcon.martin@gmail.com>
2026-01-20 11:03:51 +0100

    I´ve read chapter about commenting code, hope I did it well.

--- cipher.py
+++ cipher.py
@@ -1,15 +1,26 @@
 # trying to implement a cyclic cipher 
-new_phrase =""
+new_phrase ="" #initializes as string
 print ("This program will encript a phrase using simple cycle ciphering")
 print("")
+
+#asking user for parameters (phrase and shift value)
 phrase = str (input("enter a phrase to encode: "))
 shift = int (input ("Enter shift Value: "))
+
 for i in range (0 , len(phrase)):
+
     #converts a letter to ascii code
     ascii_code = ord(phrase[i])
+
+    #shifts the caracter with the shifting value given
     new_ascii = ascii_code + shift
+
+    #converts back to characters
     new_letter = chr(new_ascii)
+
+    #creates new phrase
     new_phrase = new_phrase + new_letter
+#prints result    
 print ("the new phrase is", new_phrase)
 
 #I think this works better that I intended 
